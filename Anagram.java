c754020295c41ad480a6e408d4dc577f8c518a5f
unknown <saiprakashbeemari@gmail.com>
2022-03-08 23:41:24 +0530

    Anagram

---
+++ Anagram.java
@@ -0,0 +1,50 @@
+package com.bl;
+
+import java.util.Arrays;
+
+public class Anagram 
+{
+	
+		 
+	    /* function to check whether two strings are
+	    anagram of each other */
+	    static boolean Anagram(char[] str1, char[] str2)
+	    {
+	        // Get lengths of both strings
+	        int n1 = str1.length;
+	        int n2 = str2.length;
+	 
+	        // If length of both strings is not same,
+	        // then they cannot be anagram
+	        if (n1 != n2)
+	            return false;
+	 
+	        // Sort both strings
+	        Arrays.sort(str1);
+	        Arrays.sort(str2);
+	 
+	        // Compare sorted strings
+	        for (int i = 0; i < n1; i++)
+	            if (str1[i] != str2[i])
+	                return false;
+	 
+	        return true;
+	    }
+	 
+	    /* Driver Code*/
+	    public static void main(String args[])
+	    {
+	        char str1[] = { 's', 'a', 'i' };
+	        char str2[] = { 's', 'a', 'i' };
+	       
+	        // Function Call
+	        if (Anagram(str1, str2))
+	            System.out.println("The two strings are"
+	                               + " anagrams ");
+	        else
+	            System.out.println("The two strings are not"
+	                               + " anagrams");
+	    }
+	}
+
+
